d45a845c890638b65477b27fcef0573c44bdeeb0
Sarthak Goel <sarthakgoel_97@hotmail.com>
2026-01-01 13:53:48 -0800

    Lecture 4: Create CodeBreaker model and drive UI using it

--- CodeBreaker/CodeBreaker/MatchMarkers.swift
+++ CodeBreaker/CodeBreaker/MatchMarkers.swift
@@ -24,18 +24,29 @@ struct MatchMarkers: View {
                 ForEach(matches[..<mid].indices, id: \.self) { index in
                     matchMarker(peg: index)
                 }
+                
+                if(matches.count == 0) {
+                    matchMarker(peg: 0)
+                    matchMarker(peg: 1)
+                }
+                
             }
             HStack {
                 ForEach(matches[mid...].indices, id: \.self) { index in
                     matchMarker(peg: index)
+                }
+                
+                if(matches.count == 0) {
+                    matchMarker(peg: 2)
+                    matchMarker(peg: 3)
                 }
             }
         }
     }
     
     func matchMarker(peg: Int) -> some View {
-        let exactCount: Int = matches.count(where: {match in match == .exact})
-        let foundCount: Int = matches.count(where: {match in match != .nomatch})
+        let exactCount = matches.count{ $0 == .exact }
+        let foundCount = matches.count{ $0 != .nomatch }
         
         return Circle()
             .fill(exactCount > peg ? Color.primary : Color.clear)
